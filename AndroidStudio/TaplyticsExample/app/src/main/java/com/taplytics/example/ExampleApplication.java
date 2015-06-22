4172c8be145ee6b8f43fc9b8aa87cfef5b7a0b9f
vicv <victorvuc@gmail.com>
2015-06-22 16:10:50 -0700

    main activity note removal

--- AndroidStudio/TaplyticsExample/app/src/main/java/com/taplytics/example/ExampleApplication.java
+++ AndroidStudio/TaplyticsExample/app/src/main/java/com/taplytics/example/ExampleApplication.java
@@ -7,10 +7,6 @@ import com.taplytics.sdk.Taplytics;
 /**
  * Overriding the application is necessary. This is so application context is passed to the sdk, as well as to set up the sdk before any
  * other content loads.
- * <p/>
- * It is possible to do this in your application's main activity onCreate(), however for maximum functionality, instantiate Taplytics in an
- * application class.
- * <p/>
  * Do not forget to add the name descriptor to your application tag in androidmanifest.xml!
  *
  * @author vicv
